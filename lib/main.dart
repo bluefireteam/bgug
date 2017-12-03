bdf5ad4e73a03b48dca95f4f1a08b5f79e5969fe
Luan Nico <luanpotter27@gmail.com>
2017-12-03 12:39:07 -0200

    Running animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'dart:ui';
 
+import 'package:flame/components/component.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
+import 'package:flame/sprite.dart';
 import 'package:flame/components/animation_component.dart';
+import 'package:flutter/services.dart';
 
 main() async {
   Flame.audio.disableLog();
-  Flame.util.fullScreen();
+//  Flame.util.fullScreen();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft]);
 
 //  Flame.audio.loop('music.ogg');
 
@@ -17,6 +21,13 @@ main() async {
   };
 }
 
+class Floor extends SpriteComponent {
+  Floor(Size dim) : super.fromSprite(dim.width, 16.0, new Sprite('base.png')) {
+    this.x = 0.0;
+    this.y = dim.height - 16.0;
+  }
+}
+
 class Player extends AnimationComponent {
 
   Size dimension;
@@ -24,13 +35,13 @@ class Player extends AnimationComponent {
   Player(this.dimension, double x, double y) : super.sequenced(64.0, 72.0, 'player.png', 8, textureWidth: 16.0, textureHeight: 18.0) {
     this.x = x;
     this.y = y;
-    this.stepTime = 0.4;
+    this.stepTime = 0.15;
   }
 
   @override
   void update(double t) {
-    this.x += 15 * t;
-    if (x > dimension.height) {
+    this.x += 30 * t;
+    if (x > dimension.width) {
       x = 0.0;
     }
     super.update(t);
@@ -43,8 +54,11 @@ class MyGame extends BaseGame {
   start() async {
     super.start();
 
-    Size dimension = await Flame.util.initialDimensions();
-    this.components.add(new Player(dimension, 200.0, 200.0));
+    // TODO consider both fullScreen and orientation for getDimensions
+    Size _dimension = await Flame.util.initialDimensions();
+    Size dimension = new Size(_dimension.height, _dimension.width);
+    this.components.add(new Floor(dimension));
+    this.components.add(new Player(dimension, 0.0, dimension.height - 72.0 - 16.0));
   }
 
   input(double x, double y) {
