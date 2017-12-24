07d8ecc83a24426f15b75052e437113aca05cc21
Luan Nico <luanpotter27@gmail.com>
2017-12-24 13:15:25 -0200

    camera and physics

--- lib/game.dart
+++ lib/game.dart
@@ -8,26 +8,33 @@ import 'package:flame/game.dart';
 import 'package:flame/sprite.dart';
 
 class Floor extends SpriteComponent {
-
-  Floor() : super.fromSprite(1.0, 16.0, new Sprite('base.png')) {
+  Floor(double width) : super.fromSprite(1.0, 16.0, new Sprite('base.png')) {
     this.x = 0.0;
+    this.width = width;
   }
 
   @override
   void resize(Size size) {
     this.y = size.height - 16.0;
-    this.width = size.width;
+  }
+}
+
+class Top extends SpriteComponent {
+  Top(double width) : super.fromSprite(1.0, 16.0, new Sprite('base.png')) {
+    this.x = 0.0;
+    this.y = 0.0;
+    this.width = width;
   }
 }
 
 class Player extends AnimationComponent {
-
   Point velocity = new Point(60.0, 0.0);
   Impulse impulse = new Impulse(20000.0);
-  double y0;
-  Size size = new Size(1.0, 1.0);
+  double y0, worldSize;
 
-  Player(double x, double y) : super.sequenced(64.0, 72.0, 'player.png', 8, textureWidth: 16.0, textureHeight: 18.0) {
+  Player(double x, double y, this.worldSize)
+      : super.sequenced(64.0, 72.0, 'player.png', 8,
+            textureWidth: 16.0, textureHeight: 18.0) {
     this.x = x;
     this.y = y;
     this.y0 = 0.0;
@@ -46,7 +53,6 @@ class Player extends AnimationComponent {
     if (y > y0) {
       y = y0;
       velocity.y = 0.0;
-
     }
     super.update(t);
   }
@@ -57,24 +63,28 @@ class Player extends AnimationComponent {
 
   @override
   bool destroy() {
-    return x > size.width;
+    return x > worldSize;
   }
 
   @override
   void resize(Size size) {
-    this.size = size;
     y0 = size.height - 72.0 - 16.0;
   }
 
   void jump() {
     if (!falling()) {
       impulse.impulse(0.1);
-    }
+    } else
+      print('Can\'t jump in the air, mate');
   }
 }
 
 class MyGame extends BaseGame {
 
+  static const double WORLD_SIZE = 1000.0;
+
+  Size size;
+  Point camera = new Point(0.0, 0.0);
   bool _running = false;
 
   bool isRunning() {
@@ -86,10 +96,10 @@ class MyGame extends BaseGame {
   }
 
   start() async {
-    this.components.add(new Floor());
-    this.components.add(new Player(0.0, 0.0));
+    this.components.add(new Top(WORLD_SIZE));
+    this.components.add(new Floor(WORLD_SIZE));
+    this.components.add(new Player(0.0, 0.0, WORLD_SIZE));
 
-//    Flame.audio.loop('music.ogg');
     this._running = true;
   }
 
@@ -108,15 +118,40 @@ class MyGame extends BaseGame {
     }
 
     super.update(dt);
+    cameraFollow(getPlayer());
 
     if (getPlayer() == null) {
       this.setRunning(false);
     }
   }
 
+  @override
+  void render(Canvas canvas) {
+    canvas.save();
+    components.forEach((comp) {
+      canvas.translate(-camera.x, -camera.y);
+      comp.render(canvas);
+      canvas.restore();
+      canvas.save();
+    });
+    canvas.restore();
+  }
+
+  void cameraFollow(Player c) {
+    if (c != null) {
+      camera.x = c.x - size.width / 2 + c.width/2;
+      if (camera.x < 0.0) {
+        camera.x = 0.0;
+      } else if (camera.x > WORLD_SIZE - size.width) {
+        camera.x = WORLD_SIZE - size.width;
+      }
+    }
+  }
+
   // TODO BaseGame should do this
   @override
   void resize(Size size) {
+    this.size = size;
     this.components.forEach((c) => c.resize(size));
   }
 }
