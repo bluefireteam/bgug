b3e66fa32238ef476b93545d7ccc084df2cb33f2
Luan Nico <luanpotter27@gmail.com>
2019-01-05 19:40:40 -0200

    surprise for erick

--- lib/components/coin.dart
+++ lib/components/coin.dart
@@ -1,18 +1,44 @@
 import 'dart:ui';
 
+import 'package:flame/animation.dart';
 import 'package:flame/components/animation_component.dart';
 
 import '../sfx.dart';
 import '../constants.dart';
 import '../mixins/has_game_ref.dart';
 
-class Coin extends AnimationComponent with HasGameRef {
-  bool collected = false;
-
-  Coin(double x, double y) : super.sequenced(1.0, 1.0, 'coin.png', 10, textureWidth: 16.0, textureHeight: 16.0) {
+class _BaseCoin extends AnimationComponent {
+  _BaseCoin(double x, double y) : super.sequenced(1.0, 1.0, 'coin.png', 10, textureWidth: 16.0, textureHeight: 16.0) {
     this.x = x;
     this.y = y;
-    this.animation.stepTime = .150;
+  }
+
+  @override
+  void resize(Size size) {
+    width = height = 0.8 * sizeTenth(size);
+  }
+}
+
+class _ExcitedCoin extends _BaseCoin {
+  _ExcitedCoin(double x, double y) : super(x, y) {
+    animation.stepTime = .005;
+    List<Frame> newFrames = [];
+    for (int i = 0; i < 5; i++) {
+      newFrames.addAll(animation.frames);
+    }
+    animation.frames = newFrames;
+    animation.loop = false;
+  }
+
+  @override
+  bool destroy() => animation.done();
+}
+
+class Coin extends _BaseCoin with HasGameRef {
+  bool collected = false;
+
+  Coin(double x, double y) : super(x, y) {
+    this.animation.stepTime = .15;
   }
 
   @override
@@ -23,12 +49,8 @@ class Coin extends AnimationComponent with HasGameRef {
       this.collected = true;
       gameRef.currentCoins++;
       Sfx.play('gem_collect.wav');
+      gameRef.addLater(_ExcitedCoin(x, y));
     }
-  }
-
-  @override
-  void resize(Size size) {
-    width = height = 0.8 * sizeTenth(size);
   }
 
   @override
