0b9e0c5cff33c2139e20545867f45ff33c94d1f4
Luan Nico <luanpotter27@gmail.com>
2019-01-06 16:00:38 -0200

    fixes

--- lib/screens/credits_screen.dart
+++ lib/screens/credits_screen.dart
@@ -1,42 +1,28 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter/gestures.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-import '../main.dart';
-import '../game.dart';
 import 'gui_commons.dart';
 
 const CREDITS = [
-	["Game developed by ", "Fireslime Team", "https://fireslime.xyz"],
-	["Music composed by ", "Joshua McLean", "http://mrjoshuamclean.com/"],
-	["Audio effects from ", "Mrthenoronha", "https://freesound.org/people/Mrthenoronha/sounds/371922/"],
-	["Base graphic assets by ", "0x72", "https://0x72.itch.io/16x16-robot-tileset"],
+  ["Game developed by ", "Fireslime Team", "https://fireslime.xyz"],
+  ["Music composed by ", "Joshua McLean", "http://mrjoshuamclean.com/"],
+  ["Audio effects from ", "Mrthenoronha", "https://freesound.org/people/Mrthenoronha/sounds/371922/"],
+  ["Base graphic assets by ", "0x72", "https://0x72.itch.io/16x16-robot-tileset"],
 ];
+
 _launchURL(String url) async {
   if (await canLaunch(url)) {
     await launch(url);
-   } else {
+  } else {
     throw 'Could not launch $url';
-   }
+  }
 }
+
 class CreditsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    if (Main.game != null) {
-      if (Main.game.state != GameState.STOPPED) {
-        return WillPopScope(
-          onWillPop: () async {
-            return await Main.game.willPop();
-          },
-          child: Main.game.widget,
-        );
-      } else {
-        Main.game = null;
-      }
-    }
-
     return Container(
       decoration: BoxDecoration(
         image: DecorationImage(
@@ -59,28 +45,25 @@ class CreditsScreen extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.center,
                 ),
                 Column(
-                  children: CREDITS.map((credit) =>
-                    Center(
-        						  child: new RichText(
-        						    text: new TextSpan(
-        						      children: [
-        						        new TextSpan(
-        						          text: credit[0],
-        						          style: black_medium_text
-        						        ),
-        						        new TextSpan(
-        						          text: credit[1],
-        						          style: medium_link,
-        						          recognizer: new TapGestureRecognizer()
-        						            ..onTap = () {
-																	_launchURL(credit[2]);
-        						            },
-        						        ),
-        						      ],
-        						    ),
-											),
-										)
-                  ).toList(),
+                  children: CREDITS
+                      .map((credit) => Center(
+                            child: RichText(
+                              text: TextSpan(
+                                children: [
+                                  TextSpan(text: credit[0], style: black_medium_text),
+                                  TextSpan(
+                                    text: credit[1],
+                                    style: medium_link,
+                                    recognizer: TapGestureRecognizer()
+                                      ..onTap = () {
+                                        _launchURL(credit[2]);
+                                      },
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ))
+                      .toList(),
                   mainAxisAlignment: MainAxisAlignment.center,
                 ),
               ],
