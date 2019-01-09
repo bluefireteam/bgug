bc14c99d4b3df113cdb45a8c829d19c8d4e20e34
Erick Zanardo <erickzanardoo@gmail.com>
2019-01-09 21:23:51 -0200

    Fixing music credit information

--- lib/screens/credits_screen.dart
+++ lib/screens/credits_screen.dart
@@ -6,10 +6,11 @@ import 'package:url_launcher/url_launcher.dart';
 import 'gui_commons.dart';
 
 const CREDITS = [
-  ["Game developed by ", "Fireslime Team", "https://fireslime.xyz"],
-  ["Music composed by ", "Joshua McLean", "http://mrjoshuamclean.com/"],
-  ["Audio effects from ", "Mrthenoronha", "https://freesound.org/people/Mrthenoronha/sounds/371922/"],
-  ["Base graphic assets by ", "0x72", "https://0x72.itch.io/16x16-robot-tileset"],
+  ["- Game developed by ", "Fireslime Team", "https://fireslime.xyz"],
+  ["- Contains music ©2018 Joshua McLean ", "(mrjoshuamclean.com)", "http://mrjoshuamclean.com/"],
+  ["Licensed under Creative Commons Attribution-ShareAlike 4.0 International"],
+  ["- Audio effects from ", "Mrthenoronha", "https://freesound.org/people/Mrthenoronha/sounds/371922/"],
+  ["- Base graphic assets by ", "0x72", "https://0x72.itch.io/16x16-robot-tileset"],
 ];
 
 _launchURL(String url) async {
@@ -33,9 +34,9 @@ class CreditsScreen extends StatelessWidget {
       child: Center(
         child: LayoutBuilder(builder: (_, size) {
           return Stack(children: [
-            Row(
+            Column(
               children: [
-                Column(
+                Row(
                   children: [
                     pad(Text('cReDiTs', style: title), 20.0),
                     btn('Go back', () {
@@ -48,17 +49,18 @@ class CreditsScreen extends StatelessWidget {
                   children: CREDITS
                       .map((credit) => Center(
                             child: RichText(
+                              textAlign: TextAlign.center,
                               text: TextSpan(
                                 children: [
                                   TextSpan(text: credit[0], style: black_medium_text),
-                                  TextSpan(
+                                  credit.length == 3 ? TextSpan(
                                     text: credit[1],
                                     style: medium_link,
                                     recognizer: TapGestureRecognizer()
                                       ..onTap = () {
                                         _launchURL(credit[2]);
                                       },
-                                  ),
+                                  ) : TextSpan(text: ""),
                                 ],
                               ),
                             ),
