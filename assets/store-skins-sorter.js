1a8f72d13ec271e1df078fa55657579219112e78
Erick Zanardo <erickzanardoo@gmail.com>
2019-01-09 23:18:09 -0200

    Sening exclusive (and Asimov) skins to the end

--- assets/store-skins-sorter.js
+++ assets/store-skins-sorter.js
@@ -5,7 +5,11 @@ const fs = require("fs");
 const originalJson = require("./store-skins.json");
 
 const sortedJson = ({
-  skins: originalJson.skins.sort((a, b) => a.cost - b.cost)
+  skins: originalJson.skins.sort((a, b) =>
+    a.cost == 0
+      ? 1
+      : a.cost - b.cost
+  )
 });
 
 fs.writeFileSync("./store-skins.json", JSON.stringify(sortedJson, null, 2), "utf8");
