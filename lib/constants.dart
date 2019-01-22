c1cf4e7a948f1c172344be6dcf687d20be1f6d6c
Luan Nico <luanpotter27@gmail.com>
2019-01-22 00:50:30 -0200

    margin bug

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,7 +7,7 @@ const bool ENABLE_ADS = true;
 
 const double BAR_SIZE = 16.0;
 const double SECTOR_LENGTH = 1000.0;
-const double SECTOR_MARGIN = 8.0;
+const double SECTOR_MARGIN = 16.0;
 
 final sizeTenth = (Size size) => (sizeBottom(size) - sizeTop(size)) / 8;
 final sizeBottom = (Size size) => size.height - BAR_SIZE;
